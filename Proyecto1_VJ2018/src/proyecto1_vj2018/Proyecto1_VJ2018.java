cf32276e52f01ceb5f7fb1216f4ff8339f37b5ce
GabrielPaz10 <39929915+GabrielPaz10@users.noreply.github.com>
2018-06-07 16:37:38 -0600

    Add files via upload

---
+++ Proyecto1_VJ2018/src/proyecto1_vj2018/Proyecto1_VJ2018.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1_vj2018;
+
+/**
+ *
+ * @author Ludwing_98
+ */
+public class Proyecto1_VJ2018 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menú menu = new Menú();
+        menu.show(true);
+    }
+    
+}
